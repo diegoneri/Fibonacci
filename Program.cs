bba2f3d8796e13bc20e9da1b5ec61cca081b5118
Diego Neri <diego@diegoneri.com>
2020-07-14 11:08:50 -0300

    Corrige o programa
    1 - Indentação (Alt + Shift + F); 2 - Linha 35 é adicionada para exibir os dois primeiros termos da sequência.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Fibonacci
 {
@@ -6,40 +6,40 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            int numero, x = 1, y = 0, z =0; // z = resultado
+            int numero, x = 1, y = 0, z = 0; // z = resultado
             bool vlvalido; // vl = valor
 
             Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(" ----- FIBONACCI ----- \n");
+            Console.WriteLine(" ----- FIBONACCI ----- \n");
 
             Console.ForegroundColor = ConsoleColor.White;
 
             Console.Write("Digite um número (inteiro >= 2): ");
-                    vlvalido = Int32.TryParse (Console.ReadLine(), out numero);
+            vlvalido = Int32.TryParse(Console.ReadLine(), out numero);
 
             if (!vlvalido || numero < 2)
 
             {
-            Console.Write("Programa finalizado: número ");
-                      Console.ForegroundColor = ConsoleColor.DarkRed;
-                              Console.WriteLine("INVÁLIDO.");
-                                      Console.ForegroundColor = ConsoleColor.White;
-                                              Console.WriteLine("Pressione enter.");
-                                                      Console.ReadKey();
-                                                              Environment.Exit(-1);
+                Console.Write("Programa finalizado: número ");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("INVÁLIDO.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Pressione enter.");
+                Console.ReadKey();
+                Environment.Exit(-1);
             }
 
-            for (int finish = 0; finish < numero; finish++) 
+            for (int finish = 0; finish < numero; finish++)
 
             {
-             z = x + y;
-             Console.WriteLine (z);
-             y = x;
-             x = z;
+                Console.WriteLine(y);
+                z = x + y;
+                y = x;
+                x = z;
             }
 
             Console.WriteLine("\nPressione enter para finalizar <3 ");
-                    Console.ReadKey();
+            Console.ReadKey();
 
         }
     }
